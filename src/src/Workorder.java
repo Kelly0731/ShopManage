35d4531d8e580fb384ecc4cc63b8eeecadd11d0d
calebkelly <kelly072@gannon.edu>
2024-04-15 13:50:30 -0400

    created workorder

---
+++ src/src/Workorder.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+public class Workorder {
+    private String customerName;
+    private String vin;
+    private Date date;
+    private String[] parts;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String[] getParts() {
+        return parts;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setParts(String[] parts) {
+        this.parts = parts;
+    }
+}
