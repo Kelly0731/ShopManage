efd7678211fe6aab7467228525b793840b95f94a
Olive <oliverjkonzel@gmail.com>
2024-04-17 13:36:46 -0400

    Worked on several things

--- src/src/Billing.java
+++ src/src/Billing.java
@@ -1,2 +1,16 @@
 public class Billing {
+    private static final double SERVICE_FEE = 50.55;
+    private final double wage;
+    private final double hoursWorked;
+
+    public Billing(double hoursWorked, double wage) {
+        this.hoursWorked = hoursWorked;
+        this.wage = wage;
+        calculateBill(hoursWorked, wage);
+    }
+
+    public double calculateBill(double hoursWorked, double wage) {
+        return hoursWorked * wage + SERVICE_FEE;
+    }
 }
+
