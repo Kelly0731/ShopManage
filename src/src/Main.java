ed94e1fa50655b88d32bec63cad08fe814951f49
calebkelly <kelly072@gannon.edu>
2024-04-21 21:04:32 -0400

    workorder print

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
         Mechanic Bob = new Mechanic("Bob", 03476);
         Workorder workorder001 = Bill.createWorkOrder(001, "Caleb", "1FTD7Z0943", new Date(2024,04,21));
         Bill.assignWorkorder(workorder001, 03476);
-
+        Bob.addPart(workorder001, "Alternator", 145783, 49.99, 129.99, "Replaced Alternator");
         Billing Jenny = new Billing(150, .30);
-        Jenny.billWorkorder(workorder001);
+        double total = Jenny.billWorkorder(workorder001);
+        System.out.println(total);
     }
 }
