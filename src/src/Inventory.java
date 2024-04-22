809cae3e66be4aa956f550898be9007278ce0508
Olive <oliverjkonzel@gmail.com>
2024-04-21 20:55:24 -0400

    Developed the Inventory system

--- src/src/Inventory.java
+++ src/src/Inventory.java
@@ -1,3 +1,35 @@
 public class Inventory {
-//Database Shit
+    private String partName;
+    private int partNumber;
+    private int quantity;
+
+    public Inventory(String partName, int partNumber, int quantity) {
+        this.partName = partName;
+        this.partNumber = partNumber;
+        this.quantity = quantity;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(int partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
---
+++ src/src/InventoryManager.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class InventoryManager {
+
+        private ArrayList<Inventory> inventories;
+
+        public InventoryManager() {
+            this.inventories = new ArrayList<>();
+        }
+
+        public void addInventory(Inventory inventory) {
+            this.inventories.add(inventory);
+        }
+
+        public void processWorkorder(Workorder workorder) {
+            String[] parts = workorder.getParts();
+            for (String part : parts) {
+                for (Inventory inventory : inventories) {
+                    if (inventory.getPartName().equals(part)) {
+                        inventory.setQuantity(inventory.getQuantity() - 1);
+                    }
+                }
+            }
+        }
+
+        public void addQuantity(String partName, int quantityToAdd) {
+            if(partName == null || quantityToAdd < 0) {
+                System.out.println("Invalid input");
+                return;
+            }
+            for (Inventory inventory : inventories) {
+                if (inventory.getPartName().equals(partName)) {
+                    inventory.setQuantity(inventory.getQuantity() + quantityToAdd);
+                }
+            }
+        }
+    }
+    //Name onCreate
+    //Has Array of INVENTORY OBJECTS
+    //When Given Work Order, moves through all parts used based on pn
+    //If part is in inventory, subtracts from quantity
+    //method to add to quantity
