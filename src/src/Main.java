f2a1581516dd84f3b7f19b0b266446fcb35eb173
calebkelly <kelly072@gannon.edu>
2024-04-21 21:24:02 -0400

    tied up main in a nice bow

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,12 +2,17 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Manager Bill = new Manager("Bill", 28974);
-        Mechanic Bob = new Mechanic("Bob", 03476);
-        Workorder workorder001 = Bill.createWorkOrder(001, "Caleb", "1FTD7Z0943", new Date(2024,04,21));
-        Bill.assignWorkorder(workorder001, 03476);
-        Bob.addPart(workorder001, "Alternator", 145783, 49.99, 129.99, "Replaced Alternator");
+        Manager Dave = new Manager("Bill", 28974);
+        Mechanic Brian = new Mechanic("Bob", 03476);
         Billing Jenny = new Billing(150, .30);
+        InventoryManager Tamara = new InventoryManager();
+        Workorder workorder001 = Dave.createWorkOrder(001, "Caleb", "1FTD7Z0943", new Date(2024,04,21));
+        Dave.assignWorkorder(workorder001, 03476);
+        Brian.addPart(workorder001, "Alternator", 145783, 49.99, 129.99, "Replaced Alternator");
+        Dave.approveWorkorder(workorder001);
+        Tamara.addInventory(new Inventory("Alternator", 145783, 4));
+        Tamara.processWorkorder(workorder001);
+        Brian.viewWorkorder(workorder001);
         double total = Jenny.billWorkorder(workorder001);
         System.out.println(total);
     }
